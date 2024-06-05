a3d8473a8dbbf0b40a879081a70a240f0102e831
NIKHIL MGRX <156079075+NIKHILBAE@users.noreply.github.com>
2024-06-05 12:31:33 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
 import platform,os
 bit = platform.architecture()[0]
 print(' CHECKING FOR UPDATES');os.system('git pull')
-try:
-    if bit == '32bit':
-        from data import NEPAL32
-    elif bit == '64bit':
-        from data import NEPAL64
-    else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
-except: 
-    if bit == '32bit':
-        import NEPAL32
-    elif bit == '64bit':
-        import NEPAL64
-    else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
-        
+if bit == '32bit':
+    from data import NEPAL32
+elif bit == '64bit':
+    from data import NEPAL64
+else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
+
